af7f59c195eb423a338378745c214edeec157941
marcewp15 <970@holbertonschool.com>
2020-11-01 23:31:11 -0500

    This format method filter values in incoming log records using filter_datum. Values for fields in fields should be filtered

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,7 +2,25 @@
 """ Filtered Logger """
 from typing import List
 import re
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Method Constructor """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Print format record """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
