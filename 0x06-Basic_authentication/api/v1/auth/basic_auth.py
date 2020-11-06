855921429e19934114c66de3401d28c5f369f610
marcewp15 <970@holbertonschool.com>
2020-11-06 22:51:28 +0000

    Update - Basic - User object: returns the User instance based on his email and password.

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,7 +37,7 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> (str, str):
-        """ Basic - User credentials: returns the user email and password
+        """ extract_user_credentials: returns the user email and password
         from the Base64 decoded value """
         if decoded_base64_authorization_header is None:
             return None, None
@@ -47,3 +47,19 @@ class BasicAuth(Auth):
             return None, None
         decoded = decoded_base64_authorization_header.split(':')
         return (decoded[0], decoded[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ user_object_from_credential: returns the User instance
+        based on his email and password """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            find_user = User.search({'email': user_email})
+            for user in find_user:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            return None
