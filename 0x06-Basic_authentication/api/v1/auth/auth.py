2704b725cfb193efd9e79155a534251610310a96
marcewp15 <970@holbertonschool.com>
2020-11-06 00:08:06 +0000

    Update auth.py - Define which routes don't need authentication (if statements)

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,14 @@ class Auth:
     """ Class Auth for API authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require authentication """
-        return False
+        if excluded_paths in [None, []]:
+            return True
+        if path == "/api/v1/status/" or path == "/api/v1/status":
+            return False
+        if path is None or path not in excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
