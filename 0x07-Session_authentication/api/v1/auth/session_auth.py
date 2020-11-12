e1ce22c8b1126cc04b4f0498fd94562c90628c92
marcewp15 <970@holbertonschool.com>
2020-11-12 01:18:00 +0000

    Update SessionAuth class

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ import uuid
 class SessionAuth(Auth):
     """ Session Authentication """
     user_id_by_session_id = {}
+
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for user_id """
         if user_id is None:
