6234348440ced8eab6924b85fe620e6e35ddd9d4
marcewp15 <970@holbertonschool.com>
2020-11-18 18:15:55 +0000

    Hash password: salted hash of the input password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """ User authentication service """
 import bcrypt
 
+
 def _hash_password(password: str) -> str:
     """ Hash password method """
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
