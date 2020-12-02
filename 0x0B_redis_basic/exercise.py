85cdba280ab091475d0e56802fc5d5663f7e107c
marcewp15 <970@holbertonschool.com>
2020-12-01 22:07:51 -0500

    0. Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Module: Redis Server, use redis for basic operations,
+    to use redis as a simple cache"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ Class: Cache"""
+    def __init__(self):
+        """ Method: Constructor Cache, initialize REDIS and flush the
+            instance using flushdb"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Method: should generate a random key with uuid4() and
+            set in redis key: value and return the key"""
+        mykey = str(uuid.uuid4())
+        self._redis.set(mykey, data)
+        return mykey
