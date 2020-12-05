95beac7fa1a582598995121247ab6d56b119706a
marcewp15 <970@holbertonschool.com>
2020-12-04 20:33:42 -0500

    Email validation to sent - This SQL script creates a trigger that resets the attribute valid_email only when the email has been changed

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- 5. Email validation to sent 
 -- This SQL script creates a trigger that resets the attribute valid_email only when the email has been changed.
 DELIMITER |
-CREATE TRIGGER StatusChangeMailTrigger
+CREATE TRIGGER validate_email
     BEFORE UPDATE ON users FOR EACH ROW
     BEGIN
          IF NEW.email <> OLD.email THEN
